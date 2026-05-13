2a9d6e1a57ec5f71e4dcae7a13dac4efba52cc3d
Pratik <pratiksharma94313@gmail.com>
2026-05-13 21:33:12 +0530

    done

---
+++ src/test/java/com/proman/PromanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
